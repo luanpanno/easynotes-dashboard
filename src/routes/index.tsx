54ee7a258355c583d94476dc41d7698e83c4f3d9
Luan Panno <luanpanno@gmail.com>
2023-02-14 04:17:21 -0300

    Create private route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,15 @@ import Dashboard from '../pages/Dashboard';
 
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
+import PrivateRoute from './PrivateRoute';
 
 const Routes = () => (
   <Switch>
     <Route path='/login' element={<Login />} />
     <Route path='/signup' element={<Signup />} />
-    <Route path='/dashboard' element={<Dashboard />} />
+    <Route path='/dashboard' element={<PrivateRoute />}>
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Route>
     <Route path='*' element={<Navigate to='/login' replace />} />
   </Switch>
 );
